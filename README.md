b55b4ac76a8f0b456b6adc5d35fc025f78215f70
Elijah Gagne <ewg@elijahg.com>
2016-05-05 19:21:39 -0400

    Project init

---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# Clog
+
+
+This is the Clog project. It is a simple chat web application running on NodeJS and MongoDB.
---
+++ server.js
@@ -0,0 +1,6 @@
+﻿var http = require('http');
+var port = process.env.port || 5000;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
